6c7d31437b8836089984ed4576f12525103805ad
bigmsg <travelcar@naver.com>
2019-11-20 13:41:44 +0900

    Initial Commit

---
+++ SwiftUI_3rd_test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_3rd_test
+//
+//  Created by 양팀장(iMac) on 2019/11/20.
+//  Copyright © 2019 양팀장(iMac). All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
